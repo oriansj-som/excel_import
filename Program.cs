372d9346849566ab4c2c8f7d6d8a23eb7fec570c
Orians, Jeremiah (DTMB) <OriansJ@michigan.gov>
2022-04-05 11:44:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Import_Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tablename = null;
+            string filename = null;
+            string databasename = "example.db";
+
+            int i = 0;
+            while (i < args.Length)
+            {
+                if (match("--file", args[i]))
+                {
+                    filename = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--table", args[i]))
+                {
+                    tablename = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--database", args[i]))
+                {
+                    databasename = args[i + 1];
+                    i = i + 2;
+                }
+                else if (match("--verbose", args[i]))
+                {
+                    int index = 0;
+                    foreach (string s in args)
+                    {
+                        Console.WriteLine(string.Format("argument {0}: {1}", index, s));
+                        index = index + 1;
+                    }
+                    i = i + 1;
+                }
+                else
+                {
+                    i = i + 1;
+                }
+            }
+
+            }
+
+        static public bool match(string a, string b)
+        {
+            return a.Equals(b, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
